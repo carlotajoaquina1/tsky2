5226a28d0e082912bae3f330ab562f36b35dcc72
carlasama <84520565+carlasama@users.noreply.github.com>
2023-06-30 22:42:56 -0400

    just simp.js

---
+++ simp.js
@@ -0,0 +1,10 @@
+function compareFn(a, b) {
+  if (a is less than b by some ordering criterion) {
+    return -1;
+  }
+  if (a is greater than b by the ordering criterion) {
+    return 1;
+  }
+  // a must be equal to b
+  return 0;
+}
